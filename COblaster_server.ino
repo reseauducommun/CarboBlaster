a0210dab8c4153539c54f49d8313809f5f8289d2
Alexandre Castonguay <acastonguay@artengine.ca>
2016-05-09 11:17:30 -0400

    CarboBlaster
    The client and server code linking two ESP8266 powered wemos’ via UDP.
    Starts an http server on port 80 for a javascript interface to run in a
    browser for feedback about the clients.
    -=- TODO -=-
    Get the data from the CO sensor (eqcq)
    Transfer to binary (eqcq)
    keep track of the connected clients in an array (eqcq)
    Send to the clients (8) ((eqcq)
    test the lowest speed limit (alx)

---
+++ COblaster_server.ino
@@ -0,0 +1,102 @@
+
+//////////////////// S E R V E R /////////////////
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+
+
+///// De :  http://www.esp8266.com/viewtopic.php?f=32&t=5669&start=4#sthash.OFdvQdJF.dpuf ///
+extern "C" {
+#include<user_interface.h>
+}
+
+/* configuration  wifi */
+const char *ssid = "COblaster";
+
+ESP8266WebServer server(80);
+WiFiUDP Udp;
+char  ReplyBuffer[50] = "42";      // a string to send back
+
+void handleRoot() { 
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+  //String addy = server.client().remoteIP().toString();
+  //Serial.println(addy);
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");  
+}
+ 
+void loop() {
+  server.handleClient();    
+  ///// delay(2000);
+  client_status();
+  ///// delay(2000);
+
+  Serial.println("envoi UDP");
+  //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());  /// à adapter avec les résultats de client_status
+  Udp.beginPacket("192.168.4.2", 2390);
+  Udp.write(ReplyBuffer); // envoi du buffer
+  Udp.endPacket();
+  // Serial.println("fin envoi UDP");
+  delay(300);
+}
+
+void client_status()  /// This works, ty sohialsadiq!
+
+{
+
+unsigned char number_client;
+struct station_info *stat_info;
+
+struct ip_addr *IPaddress;
+IPAddress address;
+int i=1;
+
+number_client= wifi_softap_get_station_num(); // Count of stations which are connected to ESP8266 soft-AP
+stat_info = wifi_softap_get_station_info();
+
+Serial.print(" Total connected_client are = ");
+Serial.println(number_client);
+
+while (stat_info != NULL) {
+
+IPaddress = &stat_info->ip;
+address = IPaddress->addr;
+
+Serial.print("client= ");
+
+Serial.print(i);
+Serial.print(" ip adress is = ");
+Serial.print((address));
+Serial.print(" with mac adress is = ");
+
+Serial.print(stat_info->bssid[0],HEX);
+Serial.print(stat_info->bssid[1],HEX);
+Serial.print(stat_info->bssid[2],HEX);
+Serial.print(stat_info->bssid[3],HEX);
+Serial.print(stat_info->bssid[4],HEX);
+Serial.print(stat_info->bssid[5],HEX);
+
+stat_info = STAILQ_NEXT(stat_info, next);
+i++;
+Serial.println();
+
+}
+
+delay(500);
+
+} 
+
+
---
+++ carboblaster_client.ino
@@ -0,0 +1,92 @@
+/////////////////////////// C L I E N T /////////////////////
+
+/* Adapté de WiFi UDP Send and Receive String
+ This sketch wait an UDP packet on localPort using a WiFi shield.
+ When a packet is received an Acknowledge packet is sent to the client on port remotePort
+ Circuit:
+ * WiFi shield attached
+ created 30 December 2012
+ by dlf (Metodo2 srl) */
+
+#include <SPI.h>
+#include <ESP8266WiFi.h> // changement pour pointer à la bonne librairie et pas la lib wifi du arduino
+#include <WiFiUdp.h>
+
+const char* ssid = "COblaster";
+unsigned int localPort = 2390;      // local port to listen on
+
+char packetBuffer[255]; //buffer to hold incoming packet
+/// char  ReplyBuffer[50] = "42";       // a string to send back
+
+const int relayPin = D1;
+const long interval = 150;  // pause pour le relai
+
+WiFiUDP Udp;
+
+void setup() {
+    pinMode(relayPin, OUTPUT);
+  Serial.begin(115200);
+   WiFi.begin(ssid); // pas besoin de mot de passe
+  
+  // if you get a connection, report back via serial:
+  Udp.begin(localPort);
+}
+
+void loop() {
+
+  // Check if module is still connected to WiFi.
+  if (WiFi.status() != WL_CONNECTED) {
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(2000);
+      Serial.println("on réessaie de se connecter");
+    }
+  
+    // Print the new IP to Serial.
+    printWiFiStatus();
+  }
+
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) packetBuffer[len] = 0;
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+    
+    digitalWrite(relayPin, HIGH); // turn on relay with voltage HIGH
+    delay(interval);              // pause
+    digitalWrite(relayPin, LOW);  // turn off relay with voltage LOW
+    delay(interval);              // pause
+
+    //////send a reply, to the IP address and port that sent us the packet we received
+    // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    // Serial.println("merci!");
+    // COS = 42;
+    // COS.toCharArray(ReplyBuffer, 50); // conversion à un buffer
+    // Udp.write(ReplyBuffer); // envoi du buffer
+    // Udp.endPacket();
+  }
+}
+
+void printWiFiStatus() {
+
+  Serial.println("");
+  Serial.print("Connecté à: ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
